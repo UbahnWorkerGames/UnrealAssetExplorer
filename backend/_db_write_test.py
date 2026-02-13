0c8854983cf5a0ded324b31bede5f9ad6022252c
Stefan <Ubahn@Worker.Games>
2026-02-13 16:53:17 +0100

    listener added

--- backend/_db_write_test.py
+++
@@ -1,9 +0,0 @@
-﻿import sqlite3
-from pathlib import Path
-p = Path(r'f:\UE_Projects\PHP\Plugins\AssetMetaExplorerBridge\open\data\app.db')
-conn=sqlite3.connect(p)
-conn.execute('PRAGMA busy_timeout=30000')
-conn.execute("UPDATE settings SET value = value WHERE key='provider'")
-conn.commit()
-conn.close()
-print('ok')
